0990a454e500479503b489ab3408300739f4be06
mohtasmsawilh <mohtasmsawilh1@gmail.com>
2018-07-12 05:26:47 +0200

    Update json.php

--- json.php
+++ json.php
@@ -9,15 +9,15 @@ if ($DBcon->connect_error) {
     die("Connection failed: " . $DBcon->connect_error);
 }
 date_default_timezone_set("Africa/Cairo");
-mysqli_query($DBcon, "SET NAMES 'utf8'");
-mysqli_query($DBcon, "set character_set_server='utf8'");
+//mysqli_query($DBcon, "SET NAMES 'utf8'");
+//mysqli_query($DBcon, "set character_set_server='utf8'");
 include "function.php";
 $Gapp = isv("app");
 if($Gapp == "login"){
     $S = Sel('Client','where username="'.Cstr(isv("user")).'" and password="'.Cstr(isv("password")).'"  and  active=1 ');
     if(!$S->serial)
     UpDate("Client","serial",isv("serial"),"where id=".$S->id);
-    die(json_encode(array("data"=>(($S && isv("serial") ==  $S->serial?$S:false))))); 
+   die(json_encode(array("data"=>(($S && isv("serial") ==  $S->serial)?$S:false),"serial"=>$S)));    
    
 }
 
