8b1a538bf8d5ab61d049f9858e7943a2d7889dd3
mohtasmsawilh <mohtasmsawilh1@gmail.com>
2018-07-12 03:54:33 +0200

    Update json.php

--- json.php
+++ json.php
@@ -11,5 +11,18 @@ if ($DBcon->connect_error) {
 date_default_timezone_set("Africa/Cairo");
 mysqli_query($DBcon, "SET NAMES 'utf8'");
 mysqli_query($DBcon, "set character_set_server='utf8'");
+include "function.php";
+$Gapp = isv("app");
+if($Gapp == "login"){
+    $S = Sel('client','where username="'.Cstr(isv("user")).'" and password="'.Cstr(isv("password")).'"  and  active=1 ');
+    die(json_encode(array("data"=>$S))); 
+   
+}
+
+
+
+
+
+
 
 ?>
